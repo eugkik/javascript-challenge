808cdfb4a317847bd9cdfd7c06a033ece8edba8c
Eugene Kikuchi <eki326@ads.northwestern.edu>
2021-10-02 21:11:14 -0500

    commit level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,38 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// create table with all UFO data
+var table = d3.select("#ufo-table");
+var tbody = table.select("tbody");
+
+tableData.forEach(function(ufoData) {
+    var row = tbody.append("tr");
+    Object.entries(ufoData).forEach(function([key, value]) {
+        var cell = row.append("td");
+        cell.text(value);
+  });
+});
+
+// create event handler
+var button = d3.select("#filter-btn");
+button.on("click", runFilter);
+
+// create filter for date
+function runFilter() {
+    var inputDateElement = d3.select("#datetime");
+    var inputDate = inputDateElement.property("value");
+    var filteredData = tableData.filter(d => d.datetime === inputDate);
+    
+    // clear table data
+    tbody.html("");
+
+    // create table with filtered data
+    filteredData.forEach(function(ufoData) {
+        var row = tbody.append("tr");
+            Object.entries(ufoData).forEach(function([key, value]) {
+            var cell = row.append("td");
+            cell.text(value);
+  });
+});
+}
+
